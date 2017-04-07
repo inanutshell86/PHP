ce2c1d2305a50327978c9e7705e28952e9e99da5
Ihor Khrystiuk <inanutshell86@gmail.com>
2017-04-07 08:43:12 +0300

    watch video about a php file structure

---
+++ php_structure/example.php
@@ -0,0 +1,48 @@
+<?php
+
+// Comment
+
+/*Multi
+Line
+Comment*/
+
+### Statements
+
+/* namespace App; */
+
+;
+;;;
+return 3;
+return 5 +5;
+// continue; break;
+
+### Expression Statements
+
+' ';
+3; //1 + 2;
+'3';
+1 + 234 / 5;
+$name = "value";
+
+### Statements list
+
+function name()
+{
+  1 + 234 / 5;
+  $name = "value";
+  return $name;
+}
+
+if ($name) {
+  $name = "value";
+  $y = 3 ** 3;
+}
+
+function ($a, $b) {
+  return $a ** $b;
+};
+
+### Side-Effects
+
+1 + 234 / 5;
+echo 1 + 234 / 5;
